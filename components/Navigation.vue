c25730cc61687ab43866141511116f6587e92c63
Aethrexal <contact@aethrexal.dev>
2024-11-05 23:46:53 +0100

    Styling with tailwindcss and UI compontents with DaisyUI. Added navigation component

---
+++ components/Navigation.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="navbar bg-base-200 md:hidden">
+    <div class="flex-1">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">MeTra</NuxtLink>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <NuxtLink to="/" :class="{ active: route.path === '/' }"
+            >Dashboard</NuxtLink
+          >
+        </li>
+        <li>
+          <NuxtLink to="/search" :class="{ active: route.path === '/search' }"
+            >Search</NuxtLink
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- Medium Screen --->
+  <div class="hidden md:block bg-base-200 w-56 h-screen pt-4">
+    <div class="text-center">
+      <NuxtLink to="/" class="text-xl font-bold">MeTra</NuxtLink>
+    </div>
+    <ul class="menu">
+      <li>
+        <NuxtLink to="/" :class="{ active: route.path === '/' }">
+          <Icon name="tabler:layout-dashboard" size="32" />
+          Dashboard
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/search" :class="{ active: route.path === '/search' }">
+          <Icon name="tabler:search" size="32" />
+          Search
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import daisyui from "daisyui";
+
+module.exports = {
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["synthwave"],
+    styled: true,
+  },
+};
